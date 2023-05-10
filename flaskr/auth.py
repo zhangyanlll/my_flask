c63d58e31d41c136aa221ccb74bce207669eb594
yan zhang <3059544830@qq.com>
2023-05-10 02:43:31 +0000

    SAVE1

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -57,7 +57,7 @@ def login():
         if error is None:
             session.clear()
             session['user_id'] = user['id']
-            return redirect('/hello')
+            return redirect(url_for('index'))
 
         flash(error)
 
@@ -77,7 +77,7 @@ def load_logged_in_user():
 @bp.route('/logout')
 def logout():
     session.clear()
-    return redirect('/auth/login')
+    return redirect(url_for('index'))
 
 def login_required(view):
     @functools.wraps(view)
